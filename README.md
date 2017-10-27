81767c7d89837f492ae91d4d484d88a418532c31
Romain Pessia <romain.cpp@gmail.com>
2017-10-27 17:08:35 +0900

    README and user input
    - Added information on how to use the program in the README.md file;
    - Added command line input in the source file.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Hohmann and bi-elliptical transfer energy efficiency
+
+This repository was created for the mid-term assignment A1 of Professor Genya Ishigami's Space Exploration Engineering course of Fall 2017, at Keio University. However, you can use it as freely as the attached license allows.
+
+## How to use the programs
+
+The c++ source file calculates the normalized speed difference of two classic orbit transfer methods: the Hohmann transfer and the bi-elliptic transfer. To that effect, the user can choose the values of the following parameters:
+
+- **min_r21**, i.e. the minimum ratio between the radii of the final and initial orbit (unit: none, type: double);
+- **max_r21**, i.e. the maximum ratio between the same radii (unit: none, type: double);
+- **num_samples**, i.e. the number of samples uniformly taken between min_r21 and max_r21.
+- **transfer_radii_ratios**, i.e. the list of all values of r_m1 (the ratio between the distance of the transfer point between the two elliptic orbits and the radius of the first orbit) to consider during the test (unit: none, type; vector<double>).
+- **short_filename**, i.e. whether the output filenames should be shortened. Setting its value to false is recommended when several entries of transfer_radii_ratios have the same floored value (unit: none, type: bool). 
+
+The user can either set those parameters directly in the source code or as arguments in the command line execution of the program (in the same order as above).
+
+After the calculation, the program creates a number of .txt files in the same folder as the executabe file:
+- **output_hohmann.txt** gives the energy efficiency of the Hohmann trasnfer for**num_samples** values of r_21 between its minimum and maximum set values.
+- **output_bi_ellipticxxx.txt** gives the energy efficiency of the bi-elliptic transfer for those same values, with a value of r_m1 equal to xxx. If **short_filename** is set to false, the filename will always include decimals.
--- source/main.cpp
+++ source/main.cpp
@@ -31,19 +31,56 @@ int main(int argc, char* argv[])
 	uint32_t num_samples = 100;
 
 	// Vector of transfer radii ratio to test for the bi-elliptic transfer
-	vector<double> transfer_radii_ratio =
+	vector<double> transfer_radii_ratios =
 	{ 5., 10., 15.58,  20., 40., 60., 80., 100., 150., 200., 500., 1000., 10000. };
 
-	// Gravitational parameter of the Earth, in m^3.s^(-2)
-	double mu = 3.986*pow(10, 14);
 
-	//_________CALCULATING ENERGY EFFICIENCY__________
+	// Boolean variable for short file naming (set to true if no two transfer radii ratio have the same 
+	// floored value)
+	bool short_filename = true;
 
-	if (num_samples <= 1)
+	if (argc > 5)
 	{
-		cout << "Error: please set a number of samples at least equal to 2.";
+		min_r21 = stod(argv[0]);
+		max_r21 = stod(argv[1]);
+		num_samples = stoi(argv[2]);
+		short_filename = stoi(argv[3]);
+		transfer_radii_ratios.clear();
+		for (int32_t i = 4; i < argc - 1; ++i)
+			transfer_radii_ratios.push_back(stod(argv[i]));
+	}
+	try
+	{
+		if (num_samples <= 1)
+		{
+			throw("The number of samples must be at least equal to 2.");
+		}
+		if (max_r21 <= min_r21)
+		{
+			throw("The maximum ratio between r2 and r1 must be greater than the minimum ratio.");
+		}
+		if (min_r21 <= 0.)
+		{
+			throw("The ratio between r2 and r1 must be non-negative.");
+		}
+
+		if (transfer_radii_ratios.empty())
+		{
+			throw("You must input at least one transfer radii ratio.");
+		}
+		for (auto ratio : transfer_radii_ratios)
+		{
+			if (ratio <= 0)
+				throw("The ratios between rm and r1 must all be non-negative.");
+		}
+	}
+	catch (string error)
+	{
+		cout << "Error: " << error;
 		return 1;
 	}
+	
+	//_________CALCULATING ENERGY EFFICIENCY__________
 
 	// Hohmann transfer energy efficiency
 	vector<vector<double>>hohmann_energy_efficiencies;
@@ -68,7 +105,7 @@ int main(int argc, char* argv[])
 
 	// Bi-elliptic transfer energy efficiency
 	// Calculation for each transfer radii ratio
-	for (double rm1 : transfer_radii_ratio)
+	for (double rm1 : transfer_radii_ratios)
 	{
 		vector<vector<double>>bi_elliptic_energy_efficiencies;
 		bi_elliptic_energy_efficiencies.clear();
@@ -84,7 +121,11 @@ int main(int argc, char* argv[])
 		// All (r21, energy_efficiency) points for the bi-elliptic transfer
 		// (with the current transfer radii ratio)
 		ofstream output_file_bi_elliptic;
-		string file_name = "output_bi_elliptic" + to_string(static_cast<uint32_t>(rm1)) + ".txt";
+		string file_name;
+		if(short_filename)
+			file_name = "output_bi_elliptic" + to_string(static_cast<uint32_t>(rm1)) + ".txt";
+		else
+			file_name = "output_bi_elliptic" + to_string(rm1) + ".txt";
 		output_file_bi_elliptic.open(file_name);
 		for (uint32_t i = 0; i < num_samples; ++i)
 		{
